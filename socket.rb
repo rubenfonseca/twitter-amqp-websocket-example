fd5416239109693b3378a2ae19955e245a943821
Ruben Fonseca <root@cpan.org>
2009-12-30 10:12:00 +0000

    we now use uuid gem to randomize client queue name

--- socket.rb
+++ socket.rb
@@ -1,16 +1,16 @@
 require 'vendor/gems/environment'
 require 'em-websocket'
+require 'uuid'
 require 'mq'
+
+uuid = UUID.new
 
 EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
   ws.onopen do
     puts "WebSocket opened"
 
     twitter = MQ.new
-
-    # randomize the queue name that binds to the fanout exchange so every
-    # websocket client (or clients that reload) get their own queue.
-    twitter.queue(rand(65000).to_s).bind(twitter.fanout('twitter')).subscribe do |t|
+    twitter.queue(uuid.generate).bind(twitter.fanout('twitter')).subscribe do |t|
       ws.send t
     end
   end
