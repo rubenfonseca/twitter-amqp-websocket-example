bf58ce6707862738325fd80d2ad4baec83e58a3b
Renaud (Nel) Morvan <nel@w3fu.com>
2012-08-07 19:14:33 +0200

    Upgrade version of amqp and get rid of raceconditions

--- twitterfeed.rb
+++ twitterfeed.rb
@@ -1,20 +1,21 @@
-require 'vendor/gems/environment'
-require 'mq'
+require 'amqp'
 require 'twitter/json_stream'
 
 username = ARGV.shift
 password = ARGV.shift
 raise "need username and password" if !username or !password
 
-AMQP.start(:host => 'localhost') do
-  twitter = MQ.new.fanout('twitter')
-  
-  stream = Twitter::JSONStream.connect(
-    :path => '/1/statuses/filter.json?track=iphone',
-    :auth => "#{username}:#{password}"
-  )
-  
-  stream.each_item do |status|
-    twitter.publish(status)
+AMQP.start(:host => 'localhost') do |connection, open_ok|
+  AMQP::Channel.new(connection) do |channel, open_ok|
+    twitter = channel.fanout("twitter")
+
+    stream = Twitter::JSONStream.connect(
+      :path => '/1/statuses/filter.json?track=iphone',
+      :auth => "#{username}:#{password}"
+    )
+
+    stream.each_item do |status|
+      twitter.publish(status)
+    end
   end
 end
